c49faa72a89140e2088478ec39f612600ea53f86
Aurore <aurore.caps.web@gmail.com>
2025-11-21 10:47:06 +0100

    img passées en png pour alourdir

--- js/extension.js
+++ js/extension.js
@@ -2,32 +2,32 @@ const extension = [
     {
         id: 1,
         alt:"maçon aigues mortes david barry extension et beton aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-maison-au-debut.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-maison-au-debut.png"
     },
     {
         id: 2,
         alt:"maçon aigues mortes david barry mur d enceinte maison aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-poutre.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-poutre.png"
     },
     {
         id: 3,
         alt:"maçon aigues mortes david barry mur maison aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-mur.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-mur.png"
     },
     {
         id: 4,
         alt:"maçon aigues mortes david barry emplacement fenetre maison aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-fenetre.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-fenetre.png"
     },
     {
         id: 5,
         alt:"maçon aigues mortes david barry mur exterieur fini maison aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-mur-fini.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-mur-fini.png"
     },
     {
         id: 5,
         alt:"maçon aigues mortes david barry charpente bois maison aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-charpente-bois.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-charpente-bois.png"
     },
     {
         id: 6,
@@ -37,27 +37,27 @@ const extension = [
     {
         id: 7,
         alt:"maçon aigues mortes david barry mur fini maison aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-creation-etage.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-creation-etage.png"
     },
         {
         id: 8,
         alt:"maçon aigues mortes david barry mur toit tuiles aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-tuiles-toit.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-tuiles-toit.png"
     },
     {
         id: 9,
         alt:"maçon aigues mortes david barry mur creation plancher aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-metrage-mise-a-niveau.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-metrage-mise-a-niveau.png"
     },
     {
         id: 10,
         alt:"maçon aigues mortes david barry mur plancher aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-plancher-maison.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-plancher-maison.png"
     },
     {
         id: 11,
         alt:"maçon aigues mortes david barry mur enduit facade aigues mortes",
-        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-facade-exterieure.jpg"
+        photo:"images/slick-images/artisan-macon-aigues-mortes-david-barry-maconnerie-facade-exterieure.png"
     },
     {
         id: 12,
